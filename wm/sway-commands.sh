e0074e5b3eb4a412ef16554f861dd7b678f33326
Eitan Traurig <eitant13@gmail.com>
2023-07-28 16:39:17 -0400

    Hyprland install script. Move EWW script

--- wm/sway-commands.sh
+++ wm/sway-commands.sh
@@ -1,33 +1,22 @@
 #!/usr/bin/env bash
 
+# PACKAGE INCLUDES build-essential.
+
+sudo apt install -y build-essential
+
+
 # Remove the EXTERNALLY_MANAGER file so pip works
 sudo rm /usr/lib/python3.11/EXTERNALLY-MANAGED
 
 # Dependencies
-sudo apt install -y sway swaylock swayidle swatbg
+sudo apt install -y sway swaylock swayidle swaybg
 
-# Eww Install
-
-#Dependecies
-sudo apt install -y rustc libgdk3.0-cil libgtk-3-0 gtk-layer-shell libpango-1.0-0 libgdk-pixbuf-2.0-0 libcairo2 libcairo-gobject2 libglib2.0-0 libglib2.0-dev libgcc-s1 glibc
-
-#Rustup
-curl https://sh.rustup.rs -sSf | sh
+mkdir -p ~/.config/sway
+cp /etc/sway/config ~/.config/sway/
+$EDITOR ~/.config/sway/config
 
 
-#Source
-git clone https://github.com/elkowar/eww
 
-cd eww
 
-cargo build --release --no-default-features --features=wayland
-
-cd target/release
-chmod +x ./eww
-./eww daemon
-
-mkdir ~/.config/eww
-cd ~/.config/eww
-touch eww.yuck eww.scss
 
 
